b3032c52a29826ce55b5c30e7d885181fdf029e2
sahadatds <sahadathossensajib531@gmail.com>
2025-11-29 14:14:25 +0600

    added

---
+++ Jarvis_Voice_App/main.py
@@ -0,0 +1,6 @@
+import os
+import eel
+# Initialize Eel Module
+eel.init("www")
+os.system('start msedge.exe --app "http:localhost:8000/index.html"')
+eel.start('index.html', mode=None, host="localhost",block=True)
